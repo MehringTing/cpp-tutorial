65a98027993e93934bcd79aae8e7e2fa324e0e57
MehringTing <dmhjxxg@163.com>
2023-02-21 21:59:32 +0800

    fetch api data

--- res/components/card.js
+++ res/components/card.js
@@ -2,6 +2,7 @@ export default class Card extends Element {
     business;
 
     constructor(props, kids) {
+        console.log('props:',props)
         super();
         this.props = props;
         this.business = props.business;
@@ -25,13 +26,13 @@ export default class Card extends Element {
         return <div class="card" styleset={__DIR__ + "../css/component.css#card"}>
             <div class="card-body">
                 <div class="play-count">
-                    <i></i>136万
+                    <i></i>{parseInt(this.props.data.playCount / 10000, 10)}万
                 </div>
                 <div class="daily" style="padding: 5dip">根据您的音乐口味生成每日更新</div>
-                <picture src="https://lqimg.dftoutiao.com/news/20230112/20230112075135_fd81a057e22d5f60dbbdec5d49e89c6f_1.jpeg" />
+                <picture src={this.props.data.picUrl} />
                 <div class="play">播放</div>
             </div>
-            <p style="padding-top: 8dip;">2022超好听的网络流行歌曲</p>
+            <p style="padding-top: 8dip;">{this.props.data.name}</p>
        </div>;
     }
 }
--- res/views/find/find.js
+++ res/views/find/find.js
@@ -7,11 +7,37 @@ import PodcastCard from "../../components/podcast-card";
  * 发现音乐
  */
 export default class Find extends Element {
+    arr = [];
+    a;
     constructor() {
         super();
+        console.log('find contructor')
+    }
+
+    async getArr() {
+        const resp = await fetch('http://localhost:3000/personalized?limit=10')
+        console.log(resp);
+        console.log(resp.ok)
+        console.log(resp.json())
+        if (resp.ok) {
+            const data = await resp.json();
+            this.arr = data.result;
+
+            const a = this.arr.map((item) => {
+                return <Card data={item} />
+            });
+
+            this.componentUpdate({a: a});
+        }
     }
 
     render() {
+        this.getArr();
+        // console.log('this.arr:', this.arr)
+        // const a = this.arr.map((item) => {
+        //     console.log('item:', item)
+        //     return <Card data={item} />
+        // });
         return <div>
                 <ul class="tabs v-tabs">
                 <li class="tab active">个性推荐</li>
@@ -25,16 +51,7 @@ export default class Find extends Element {
             <div class="grouped-list recommend">
                 <div class="name" style="padding-bottom: 10dip;">推荐歌单</div>
                 <div class="list">
-                    <Card />
-                    <Card />
-                    <Card />
-                    <Card />
-                    <Card />
-                    <Card />
-                    <Card />
-                    <Card />
-                    <Card />
-                    <Card />
+                    {this.a}
                 </div>
             </div>
 
@@ -51,11 +68,7 @@ export default class Find extends Element {
             </div>
             <div class="grouped-list exclusive">
                 <div class="name" style="padding-top: 40dip;">独家放送</div>
-                <div class="list">
-                    <Card business="exclusive" />
-                    <Card business="exclusive" />
-                    <Card business="exclusive" />
-                </div>
+
             </div>
             <div class="grouped-list newest">
                 <div class="name" style="padding-top: 40dip;">最新音乐</div>
@@ -75,7 +88,7 @@ export default class Find extends Element {
                 </div>
             </div>
             <div class="grouped-list podcasts">
-                <div class="name" style="padding-top: 40dip;">主题播客</div>
+                <div class="name" style="padding-top: 40dip; padding-bottom: 40dip;">主题播客</div>
                 <div class="list">
                     <PodcastCard />
                     <PodcastCard />
